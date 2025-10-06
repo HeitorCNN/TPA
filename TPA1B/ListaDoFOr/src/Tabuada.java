716eb3fbf48949341cb23a7b373d1c3425d09ad4
HeitorCNN <heitorcaetano2302@gmail.com>
2025-10-06 11:17:50 -0300

    Add files via upload

---
+++ TPA1B/ListaDoFOr/src/Tabuada.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Tabuada {
+     public void main(String[] args) {
+    	 
+    	 Scanner in = new Scanner(System.in);
+    	 
+    	 int n, i, r;
+    	 System.out.println("Digite o numero ");
+    	 n = in.nextInt();
+    	 
+     }
+     
+}
